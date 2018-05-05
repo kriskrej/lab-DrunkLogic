d14cbc39355eb310e989b988b6e951b832030308
Kris Krej <kris@krej.net>
2018-05-05 18:36:38 +0200

    Base project

---
+++ Assets/Code/Witcher/WitcherLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitcherLogic : MonoBehaviour {
+    public WitcherVisualisation ui;
+
+    void Update() {
+        ui.PointX = ui.MouseX;
+        if (IsMouseOverBar()) {
+            ui.BarX++;
+        }
+        else {
+            ui.BarX--;
+        }
+        //ui.BarX = ui.MouseX;
+        ui.BarWidth = (1000-ui.BarX)/4;
+
+        
+    }
+
+    bool IsMouseOverBar() {
+        return Mathf.Abs(ui.BarX - ui.MouseX) < ui.BarWidth / 2;
+    }
+
+
+    float life = 0.5f;
+	public float GetKnobPositionInNextFrame(float currentPosition01, float mousePosition01) {
+        return (currentPosition01+mousePosition01)/2;
+    }
+}
---
+++ Assets/Code/Witcher/WitcherVisualisation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitcherVisualisation : MonoBehaviour {
+    public RectTransform bar;
+    public RectTransform point;
+    public WitcherLogic logic;
+    const float DEFAULT_BAR_WIDTH = 100;
+
+    public float CanvasWidth {
+        get { return 1000; }
+    }
+
+    public float MouseX {
+        get {
+            var pos01 = Mathf.Clamp01(Input.mousePosition.x / Screen.width);
+            return pos01*CanvasWidth;
+        }
+    }
+
+    public float BarX {
+        get {
+            return bar.anchoredPosition.x + CanvasWidth/2;
+        }
+        set {
+            bar.anchoredPosition = new Vector2(value - CanvasWidth / 2, bar.anchoredPosition.y);
+        }
+    }
+
+    public float BarWidth {
+        get {
+            return DEFAULT_BAR_WIDTH*bar.localScale.x;
+        }
+        set {
+            bar.localScale = new Vector3(value / DEFAULT_BAR_WIDTH, bar.localScale.y, bar.localScale.z);
+        }
+    }
+
+    public float PointX {
+        get {
+            return point.anchoredPosition.x + CanvasWidth/2;
+        }
+        set {
+            point.anchoredPosition = new Vector2(value - CanvasWidth / 2, point.anchoredPosition.y);
+        }
+    }
+
+
+}
