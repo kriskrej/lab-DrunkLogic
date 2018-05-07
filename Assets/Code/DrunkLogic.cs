673b5e3f8cfe9b329b884461fa51c9df7cb808c3
Kris Krej <kris@krej.net>
2018-05-07 16:53:49 +0200

    Let's make life a bit harder.

--- Assets/Code/DrunkLogic.cs
+++ Assets/Code/DrunkLogic.cs
@@ -8,23 +8,7 @@ public class DrunkLogic : MonoBehaviour {
         ui.BarX = 500;
     }
 
-    float speed = 100;
-
     void Update() {
-        ui.PointX = ui.MouseX;
-        if (IsMouseOverBar()) {
-            ui.BarX+=Time.deltaTime * speed;
-        }
-        else {
-            ui.BarX-= Time.deltaTime * speed;
-        }
-        ui.BarWidth = (1000-ui.BarX)/8;
-        animator.SetFloat("Drunk", 1f-ui.BarX/1000f);
-        if(ui.BarX<1)GameManager.ShowRespawnScreen("You have lost. Everything.");
-        
-    }
 
-    bool IsMouseOverBar() {
-        return Mathf.Abs(ui.BarX - ui.MouseX) < ui.BarWidth / 2;
     }
 }
