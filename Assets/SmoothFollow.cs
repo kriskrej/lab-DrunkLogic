8f0af1e5c22a177705aa304032f0790e9578a72d
Kris Krej <kris@krej.net>
2018-05-07 15:40:25 +0200

    Some effects

---
+++ Assets/SmoothFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour {
+    public Transform target;
+    Vector3 distance;
+	void Start () {
+	    distance = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.position = target.position + distance;
+	}
+}
