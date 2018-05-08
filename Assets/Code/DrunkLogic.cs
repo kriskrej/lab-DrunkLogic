49b8681e2de8201460786cd5e46ae439f53965fd
Kris Krej <kris@krej.net>
2018-05-08 11:32:43 +0200

    More readable DrunkLogic.cs

--- Assets/Code/DrunkLogic.cs
+++ Assets/Code/DrunkLogic.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
-
+// Imię:
+// Nazwisko:
+// Nr indeksu:
 public class DrunkLogic : MonoBehaviour {
     public DrunkUI ui;
     public Animator animator;
 
     public void Start() {
+        ui.BarWidth = 100;
         ui.BarX = 500;
     }
 
